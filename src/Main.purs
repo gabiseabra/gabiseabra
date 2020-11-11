15c8637d8bbe6e1fc9a055ea225f99166e08fcb6
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-11-11 17:47:13 -0300

    404

--- src/Main.purs
+++ src/Main.purs
@@ -9,6 +9,8 @@ import Effect.Exception (throw)
 import Hey.Components.Menu (mkMenu)
 import Hey.Env (Env, mkEnv)
 import Hey.Pages.Home (mkHomePage)
+import Hey.Pages.NotFound (mkNotFoundPage)
+import Hey.Router (Route(..))
 import React.Basic.DOM (render)
 import React.Basic.Hooks (Component, JSX, component, fragment)
 import React.Basic.Hooks as React
@@ -21,9 +23,11 @@ import Wire.React (useSignal)
 mkRouter :: Component Env
 mkRouter = do
   home <- mkHomePage
+  notFound <- mkNotFoundPage
   component "Router" \env -> React.do
     route <- useSignal env.router.signal
     case route of
+      NotFound -> pure $ notFound env
       _ -> pure $ home env
 
 mkApp :: Effect JSX
