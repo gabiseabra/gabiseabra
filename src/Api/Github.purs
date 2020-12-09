e24d297d02020c7301cd6c047558b3e361115800
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-12-08 21:00:51 -0300

    github api/useFetch

---
+++ src/Api/Github.purs
@@ -0,0 +1,72 @@
+module Hey.Api.Github where
+
+import Prelude
+import Affjax as AX
+import Affjax.RequestBody as Req
+import Affjax.RequestHeader (RequestHeader(..))
+import Affjax.ResponseFormat as Res
+import Data.Argonaut (encodeJson)
+import Data.Either (Either(..))
+import Data.HTTP.Method (Method(..))
+import Data.Maybe (Maybe(..))
+import Hey.Hooks.UseFetch (Fetch(..))
+
+type Connection a
+  = { pageInfo ::
+        { hasNextPage :: Boolean
+        , endCursor :: String
+        }
+    , nodes :: Array a
+    }
+
+type Connection' a
+  = { nodes :: Array a }
+
+type Repos
+  = { viewer ::
+        { repositories ::
+            Connection
+              { name :: String
+              , description :: Maybe String
+              , isFork :: Boolean
+              , languages :: Connection' { name :: String, color :: Maybe String }
+              }
+        }
+    }
+
+fetchRepos :: Fetch { data :: Repos }
+fetchRepos = Fetch "github/repos" req
+  where
+  req =
+    AX.defaultRequest
+      { url = "https://api.github.com/graphql"
+      , headers = [ RequestHeader "Authorization" "bearer e2cdad17d8b6308897f05edaace521b1964cfb0e" ]
+      , content = Just $ Req.json $ encodeJson { query }
+      , method = Left POST
+      , responseFormat = Res.json
+      }
+
+  query =
+    """
+    query {
+      viewer {
+        repositories(first: 100) {
+          nodes {
+            name
+            description
+            isFork
+            languages(first: 3) {
+              nodes {
+                name
+                color
+              }
+            }
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+        }
+      }
+    }
+    """
