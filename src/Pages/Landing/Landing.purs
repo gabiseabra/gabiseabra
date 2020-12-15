2a826beabdb65eac7ec191b331c1cda9bb7d7fb6
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-12-15 00:02:14 -0300

    smoke

--- src/Pages/Landing/Landing.purs
+++ src/Pages/Landing/Landing.purs
@@ -5,10 +5,7 @@ module Hey.Pages.Landing
 import Prelude
 
 import Hey.Components.Hole (hole)
-import Hey.Hooks.UseViewportSize (ViewportSize, useViewportSize)
-import React.Basic (JSX)
 import React.Basic.DOM as DOM
-import React.Basic.DOM.SVG as SVG
 import React.Basic.Hooks (Component, component)
 import React.Basic.Hooks as React
 
@@ -16,15 +13,17 @@ foreign import styles :: Styles
 
 type Styles =
   { page :: String
+  , smoke :: String
   }
-
 
 mkLandingPage :: forall a . Component a
 mkLandingPage = component "Landing" \_ -> React.do
-  viewport <- useViewportSize
   pure
     $ DOM.div
     { className: styles.page
-    , children: [ hole ]
+    , children:
+      [ DOM.img { className: styles.smoke, src: "/img/smoke.gif" }
+      , hole
+      ]
     }
   
