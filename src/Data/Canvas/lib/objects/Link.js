78094f235d7759f2f6f2b816064588524b46fb00
Gabriela Seabra <gabriela.lima.se@gmail.com>
2021-03-04 13:42:46 -0300

    optimize navbar objects

--- src/Data/Canvas/lib/objects/Link.js
+++ src/Data/Canvas/lib/objects/Link.js
@@ -1,52 +1,25 @@
 import * as THREE from 'three'
 import {gsap} from 'gsap'
-import SourceCodePro from '../fonts/SourceCodePro'
 
+/* link looks & feel */
 const DURATION = 0.25
-
 const COLOR = new THREE.Color(0x73b6fa)
 const ACTIVE_COLOR = new THREE.Color(0xffbcb8)
 
 const rgb = ({r, g, b}) => ({r, g, b})
 
 export class Link extends THREE.Object3D {
-  active = false
-  chars = []
-
-  constructor(label, charWidth = 8.45) {
+  constructor(geom) {
     super()
+    const mesh = new THREE.Mesh(
+      geom,
+      new THREE.MeshPhysicalMaterial({color: COLOR})
+    )
+    this.add(mesh)
 
-    const mat = new THREE.MeshPhysicalMaterial({color: COLOR})
-    // const mat = new THREE.MeshNormalMaterial()
-
-    label.split('').forEach((char, idx) => {
-      const geom = new THREE.TextGeometry(char, {
-        font: SourceCodePro,
-        size: 10,
-        height: 5,
-        curveSegments: 8,
-        bevelEnabled: true,
-        bevelThickness: 0.8,
-        bevelSize: 0.5,
-        bevelOffset: -0.5,
-        bevelSegments: 4
-      })
-      const mesh = new THREE.Mesh(geom, mat)
-      mesh.translateX(charWidth * idx)
-
-      const pivot = new THREE.Object3D()
-      pivot.add(mesh)
-      this.add(pivot)
-      this.chars.push(mesh)
-    })
-
-    const scale = this.chars.map((c) => c.scale)
-
-    this.material = mat
     this.tween = gsap
       .timeline({defaults: {duration: DURATION}})
-      .fromTo(mat.color, rgb(COLOR), rgb(ACTIVE_COLOR), 0)
-      .fromTo(scale, {z: 1}, {z: 3}, 0)
+      .fromTo(mesh.material.color, rgb(COLOR), rgb(ACTIVE_COLOR), 0)
       .pause(0)
   }
 
--- src/Data/Canvas/lib/objects/Navbar.js
+++ src/Data/Canvas/lib/objects/Navbar.js
@@ -1,9 +1,12 @@
 import * as THREE from 'three'
+import {BufferGeometryUtils} from 'three/examples/jsm/utils/BufferGeometryUtils'
+import SourceCodePro from '../fonts/SourceCodePro'
 import {Link} from './Link'
 
-const CHAR_WIDTH = 3.45
-const SPACING = 3
-const ANGLE = Math.PI * 0.25
+/* curve modifier options */
+const CHAR_WIDTH = 3.5 // 8.45
+const SPACING = 2.5 // num of spaces between links
+const ANGLE = Math.PI * 0.3 // angle of the arc
 
 export class Navbar extends THREE.Object3D {
   refs = {}
@@ -11,52 +14,43 @@ export class Navbar extends THREE.Object3D {
   constructor(links) {
     super()
 
-    let length // number of characters in the scene including spaces
-    {
-      const spaces = links.length - 1
-      let chars = 0
+    const geomsList = links.map((l) => mkLinkGeometries(l))
+    const chars = geomsList.reduce((acc, l) => l.length + acc, 0)
+    const spaces = links.length - 1
+    const length = chars + spaces * SPACING
 
-      links.forEach(({id, label}) => {
-        const link = new Link(label, 0)
+    const curve = mkCurve(length)
 
-        this.refs[id] = link
-        this.add(link)
+    // modify and merge link geometries
+    forEach2(geomsList, curve.modifyGeometry)
+    geomsList.forEach((geoms, idx) => {
+      const link = links[idx]
 
-        chars += label.length
-      })
+      const geom = new THREE.Geometry().fromBufferGeometry(
+        BufferGeometryUtils.mergeBufferGeometries(geoms)
+      )
+      geom.translate(0, 0, curve.apothem)
 
-      length = chars + spaces * SPACING
-    }
+      const mesh = new Link(geom)
+      const spacing = idx * SPACING * CHAR_WIDTH
+      mesh.translateX(spacing)
+      mesh.name = link.id
 
-    // arc params
-    const chord = length * CHAR_WIDTH * 2
-    const radius = chord / 2 / Math.sin(ANGLE / 2)
-    const sagitta =
-      radius - Math.sqrt(Math.pow(radius, 2) - Math.pow(chord / 2, 2))
+      // var bbox = new THREE.BoundingBoxHelper(mesh, 0xff0000)
+      // bbox.update()
+      // this.add(bbox)
 
-    // center
-    this.translateX(-CHAR_WIDTH / 2)
-    // set pivot
-    this.translateZ(radius - sagitta)
+      this.refs[link.id] = mesh
+      this.add(mesh)
+    })
 
-    // iterate over each character to translate around the arc
-    this.children.reduce(
-      (i, link, idx) =>
-        link.children.reduce((j, char) => {
-          const pos = j + idx * SPACING
-          const u = pos / (length - 1)
-          const a = -ANGLE * (u - 0.5)
-          char.rotateY(a)
-          char.translateZ(-radius)
-          return j + 1
-        }, i),
-      0
-    )
+    this.translateX(-((spaces + 1) * SPACING * CHAR_WIDTH) / 2)
   }
 
   setActive(id) {
     if (this.activeId) {
       this.refs[this.activeId].setActive(false)
+      this.activeId = null
     }
     if (this.refs[id]) {
       this.refs[id].setActive(true)
@@ -64,3 +58,59 @@ export class Navbar extends THREE.Object3D {
     }
   }
 }
+
+const mkLinkGeometries = ({label}) =>
+  label.split('').map(
+    (char) =>
+      new THREE.TextBufferGeometry(char, {
+        font: SourceCodePro,
+        size: 10,
+        height: 7,
+        curveSegments: 8,
+        bevelEnabled: true,
+        bevelThickness: 0.8,
+        bevelSize: 0.5,
+        bevelOffset: -0.5,
+        bevelSegments: 4
+      })
+  )
+
+const mkCurve = (length) => {
+  // arc
+  const chord = length * CHAR_WIDTH * 2
+  const radius = chord / 2 / Math.sin(ANGLE / 2)
+  const sagitta =
+    radius - Math.sqrt(Math.pow(radius, 2) - Math.pow(chord / 2, 2))
+  const apothem = radius - sagitta
+  return {
+    chord,
+    radius,
+    sagitta,
+    apothem,
+    getOffset() {},
+    modifyGeometry(geom, j, idx0) {
+      const mesh = new THREE.Mesh(geom)
+
+      const pos = j + idx0 * SPACING
+      const u = pos / (length - 1)
+      const a = -ANGLE * (u - 0.5)
+
+      mesh.rotateY(a)
+      mesh.translateZ(-radius)
+      mesh.updateMatrix()
+
+      geom.applyMatrix(mesh.matrix)
+      geom.userData.offset = Math.cos(a) * radius
+    }
+  }
+}
+
+const forEach2 = (l, fun) =>
+  l.reduce(
+    (i, ll, idx0) =>
+      ll.reduce((j, x, idx1) => {
+        fun(x, j, idx0, idx1)
+        return j + 1
+      }, i),
+    0
+  )
