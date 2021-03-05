98ae398e6bb9749b370cedc657917ac3198fe63c
Gabriela Seabra <gabriela.lima.se@gmail.com>
2021-03-04 22:59:04 -0300

    todo

--- src/Data/Canvas/Canvas.purs
+++ src/Data/Canvas/Canvas.purs
@@ -11,6 +11,7 @@ import Web.DOM (Node)
 
 foreign import kind Scene
 
+-- TODO optimization: remove scene param and render all in a single canvas
 foreign import data Canvas :: Scene -> Type
 
 foreign import toNode :: forall a. Canvas a -> Node
