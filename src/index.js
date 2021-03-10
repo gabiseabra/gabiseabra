3e7356abe06c476486652dde2591b67b374ffbf8
Gabriela Seabra <gabriela.lima.se@gmail.com>
2021-03-09 21:20:24 -0300

    lint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import {gsap} from 'gsap'
 import {ScrollTrigger} from 'gsap/ScrollTrigger'
-import * as SMAA from './lib/3d/smaa'
 import Main from './Main'
 import './index.scss'
 
 gsap.registerPlugin(ScrollTrigger)
 
 document.addEventListener('DOMContentLoaded', () => {
-  //SMAA.init()
   Main.main()
 })
