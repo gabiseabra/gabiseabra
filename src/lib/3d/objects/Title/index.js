f9710f0c3a12a6737f93126abd13c4b699a62669
Gabriela Seabra <gabriela.lima.se@gmail.com>
2021-03-21 14:11:43 -0300

    adjust title rotation

--- src/lib/3d/objects/Title/index.js
+++ src/lib/3d/objects/Title/index.js
@@ -11,21 +11,25 @@ export class Title extends THREE.Object3D {
 
   init(mesh, {config}) {
     const pivot = new THREE.Object3D()
-    pivot.position.z = -50
     this.add(pivot)
 
-    mesh.translateY(config.sun.distance / 2).rotateX(-1.5)
     mesh.scale.x *= -1
+    mesh.translateZ(config.sun.distance / 2)
     pivot.add(mesh)
 
     this.mesh = mesh
     this.pivot = pivot
 
     this.ready = true
+    this.setProgress(0)
   }
 
   setProgress(p) {
     if (!this.ready) return
-    this.pivot.rotation.x = THREE.MathUtils.lerp(0, Math.PI / 3, p)
+    this.pivot.rotation.x = THREE.MathUtils.lerp(
+      -Math.PI / 1.95,
+      -Math.PI / 1.5,
+      p
+    )
   }
 }
